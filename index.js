cce93e3fae532b1dd9447da71331ca7d5fe8a3e7
Shehara <t2wasu00@students.oamk.fi>
2023-12-07 15:49:13 +0200

    added deployment info

--- index.js
+++ index.js
@@ -7,16 +7,14 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dataBase_1 = __importDefault(require("./dataBase"));
 const dropoff_controller_1 = __importDefault(require("./controllers/dropoff_controller"));
-const dropoff_completion_1 = __importDefault(require("./controllers/dropoff_completion"));
 const pickup_controller_1 = __importDefault(require("./controllers/pickup_controller"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use('/locker', dropoff_controller_1.default);
-app.use('/locker', dropoff_completion_1.default);
 app.use('/locker', pickup_controller_1.default);
-const port = 3001;
+const port = process.env.PORT || 3001;
 // Test database connection
 dataBase_1.default.getConnection((err) => {
     if (err) {
